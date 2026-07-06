2ecc44c69390961fc668537b92a266714a56912b
HomayoonAAhmad <homayoon5600@gmail.com>
2026-07-06 13:07:01 +0330

    add user module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,22 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Put, Req, UseGuards } from '@nestjs/common';
 import { Request } from 'express';
 import { AuthGuard } from 'src/common/guards/auth.guard';
+import { UserService } from './user.service';
 
 @Controller('user')
 @UseGuards(AuthGuard)
 export class UserController {
-  @Get()
-  sendOtp(@Req() req: Request) {
+  constructor(private readonly userService: UserService) {}
+
+  @Get('me')
+  getUserData(@Req() req: Request) {
     const id = req.user.id;
-    return id;
+    return this.userService.getUserData(id);
+  }
+
+  @Put('update')
+  updateUserData(@Req() req: Request) {
+    const id = req.user.id;
+    return this.userService.updateUserData(id);
   }
 }
